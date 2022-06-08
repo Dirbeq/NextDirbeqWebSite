934dbe4be1edd65713a61ae81a911f16e05082ea
Dirbeq <dirbeq@gmail.com>
2022-06-08 15:09:55 +0200

    up

---
+++ website/pages/playground/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head';
+import Layout from '../../components/layout';
+import styles from '../../styles/sandbox.module.css'
+
+
+export default function Sandbox() {
+    return (<Layout></Layout>
+    );
+}
